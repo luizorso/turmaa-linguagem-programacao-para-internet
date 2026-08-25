19f8fabdde4a4937a6b6340c9ab6e60dd408bdf7
luizorso <laorso@gmail.com>
2026-08-25 10:07:29 -0300

    Quarto Commit: Alteração da Classe PerfilDTO, Alteração da Classe PerfilController e adição inicial da página cadastarUsuario

--- src/main/java/com/cepestm/turmaa/controller/PerfilController.java
+++ src/main/java/com/cepestm/turmaa/controller/PerfilController.java
@@ -1,14 +1,25 @@
 
-package com.cepestm.turmaa.controller;
+package com.cepestm.turmab.controller;
 
-import com.cepestm.turmaa.service.PerfilService;
+import com.cepestm.turmab.dto.PerfilDTO;
+import com.cepestm.turmab.service.PerfilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PerfilController {
+public class PerfilController{
     
     @Autowired
     private PerfilService service;
+    
+    @GetMapping("/cadastrarPerfil")
+    public String cadastrarPerfil(Model model){
+        model.addAttribute("título", "Cadastro de Perfil");
+        model.addAttribute("perfilDTO", new PerfilDTO(""));
+        return "cadastrarPerfil";
+        
+    }
     
 }
--- src/main/java/com/cepestm/turmaa/dto/PerfilDTO.java
+++ src/main/java/com/cepestm/turmaa/dto/PerfilDTO.java
@@ -1,15 +1,22 @@
 
-package com.cepestm.turmaa.dto;
+package com.cepestm.turmab.dto;
 
-import com.cepestm.turmaa.entity.Perfil;
+import com.cepestm.turmab.entity.*;
+import java.util.UUID;
 
 
-public record PerfilDTO(String nome) {
+public record PerfilDTO(UUID id,String nome){
+    
+    public PerfilDTO(String nome){
+        this(null, nome);
+    }
     
     public Perfil toPerfil(){
         Perfil perfil = new Perfil();
+        perfil.setId(id);
         perfil.setNome(nome);
         return perfil;
+        
     }
     
 }
