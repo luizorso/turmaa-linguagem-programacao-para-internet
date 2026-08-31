d81faecc1e73321302b9e259e8e7a6f3f1f3bc46
luizorso <laorso@gmail.com>
2026-08-31 08:28:45 -0300

    Sétimo Commit:adição dos métodos listAll, listById, deleteById e updateB na classe PerfilService e adição do método salvarPerfil na classe PerfilController

--- src/main/java/com/cepestm/turmaa/controller/PerfilController.java
+++ src/main/java/com/cepestm/turmaa/controller/PerfilController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PerfilController{
@@ -22,4 +25,25 @@ public class PerfilController{
         
     }
     
+    @PostMapping("/cadastrarPerfil")
+    public String salvarPerfil(PerfilDTO dto, 
+            RedirectAttributes redAtributos){
+        if(dto.id() != null){
+            service.update(dto.id(), dto);
+            redAtributos.addFlashAttribute(
+                 "mensagem","Perfil atualizado com sucesso!");
+        }else{
+            service.saveByDTO(dto);
+            redAtributos.addFlashAttribute(
+                 "mensagem", "Perfil cadastrado com sucesso!"
+            );
+        }
+        return "redirect:/perfis";
+        
+    }
+    
+    @GetMapping("/perfis")
+    public String listarPerfis(Model model){
+        return "listarPerfis";
+    }
 }
--- src/main/java/com/cepestm/turmaa/service/PerfilService.java
+++ src/main/java/com/cepestm/turmaa/service/PerfilService.java
@@ -5,13 +5,13 @@ import com.cepestm.turmaa.dto.PerfilDTO;
 import com.cepestm.turmaa.entity.Perfil;
 import com.cepestm.turmaa.repository.PerfilRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PerfilService {
     
-   
     private final PerfilRepository rep;
     
     public PerfilService(PerfilRepository rep){
@@ -24,6 +24,45 @@ public class PerfilService {
         // executa o insert na base dados
         Perfil perfil = rep.save(dto.toPerfil());
         return perfil;
+    }
+    
+    public List<Perfil> listAll(){
+        return rep.findAll();
+    }
+     /*
+        findById(id)> retorna um objeto do tipo optional <Perfil>.
+        Usamos .orElseThrow para lançar uma exceção amigável
+        caso o Id buscado não exista no banco de dados.
+    */
+    
+    
+    public Perfil listById(UUID id){
+        return rep.findById(id)
+        .orElseThrow(() ->  new RuntimeException(
+                "Perfil não encontrado para o ID: " +  id));
+    }
+    
+    @Transactional
+    public void deleteById(UUID id){
+        //Declar um objeto perfil e atribui a ele um perfil
+        //pelo métodoListByIdm
+        Perfil perfil = listById(id);
+        //O objeto rep invoca o método delete e passa 
+        //como parâmetro o perfil
+        rep.delete(perfil);
+    }
+    
+    @Transactional
+    public Perfil update(UUID id, PerfilDTO dto){
+        //Busca o perfil existen no banco de dado
+        Perfil perfil = listById(id);
+        
+        //Atualiza os dados da entidade com as informações
+        //recebido no objeto DTO
+        perfil.setNome(dto.nome());
+        
+        //Salva a entidade atualizada (faz Update por conta do Id presente)
+        return rep.save(perfil);
     }
     
 }
